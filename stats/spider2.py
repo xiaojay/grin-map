88a2373adf6e552d153954797f494f7bfa721803
xiaojay <xiaojay@gmail.com>
2019-08-06 09:00:40 +0800

    update

--- stats/spider2.py
+++ stats/spider2.py
@@ -3,7 +3,7 @@ import requests, time, re
 urls = []
 date = []
 days = []
-for i in range(1, 6):
+for i in range(25, 32):
     for j in range(2, 26, 4):
         i_ = str(i)
         if i < 10:
@@ -12,10 +12,10 @@ for i in range(1, 6):
             j_ = '0%i_00'%j
         else:
             j_ = '%i_00'%j
-        url = 'http://grin-map.cycle42.com/data/2019/Aug/%s/%s/'%(i_, j_)
+        url = 'http://grin-map.cycle42.com/data/2019/Jul/%s/%s/'%(i_, j_)
         urls.append(url)
 
-        date.append('2019-08-%s %s'%(i, j))
+        date.append('2019-07-%s %s'%(i, j))
         days.append(i)
         #print url
 
@@ -27,9 +27,13 @@ for i, u in enumerate(urls):
     c = requests.get(u).content
     if c.find('404') != -1:
         continue
-    
-    total_count = re.findall('Total nodes:\s*(\d+)\s*', c)[0]
-    public_count = re.findall('Public nodes:\s*(\d+)\s*', c)[0]
+    try:
+        total_count = re.findall('Total nodes:\s*(\d+)\s*', c)[0]
+        public_count = re.findall('Public nodes:\s*(\d+)\s*', c)[0]
+    except IndexError:
+        print u
+        print 'failed to parse'
+        continue
     print date[i], total_count, public_count 
     t = days[i]
     if t == y:
